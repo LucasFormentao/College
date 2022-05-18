624d5ae668d09020844c1ab6ced2956c7e7cb9d4
Baccus <103602285+BaccusIsCoding@users.noreply.github.com>
2022-05-18 14:59:35 -0300

    Create Matriz Aleatória.c

---
+++ Atividades Aulas/Matriz Aleatória.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main()
+{
+    int i, j, lin=3, col=2;
+    int mat_1[lin][col];
+    //O nome da matriz é o endereço que guarda a posição [0,0]//
+    printf("Entre com os valores da matriz:\n");
+    srand(time(NULL));
+    
+    for(i=1; i<=lin;i++)
+    {
+        for(j=1;j<=col;j++)
+        {
+            mat_1[i][j] = rand()%500;
+        }
+    }
+    
+    printf("\nMatriz digitada:\n");
+    
+    for(i=1; i<=lin;i++)
+    {
+        for(j=1;j<=col;j++)
+        {
+            printf("%3d ", mat_1[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
