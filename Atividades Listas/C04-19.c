0170e71429374942dc6fcbe2d55355f5e0dc0836
Baccus <103602285+BaccusIsCoding@users.noreply.github.com>
2022-05-19 16:21:16 -0300

    Create C04-19.c

---
+++ Atividades Listas/C04-19.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int vet[50], i;
+    for(i=0; i<50; i++)
+    {
+        printf("%d ", vet[i] = (i+5*i)%(i+1));
+    }
+
+    return 0;
+}
