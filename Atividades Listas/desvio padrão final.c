7baafa3cbeb1bb5bb2fe976a29eba9d4d97c28ae
Baccus <103602285+BaccusIsCoding@users.noreply.github.com>
2022-05-19 18:16:30 -0300

    Create desvio padrão final.c

---
+++ Atividades Listas/desvio padrão final.c
@@ -0,0 +1,43 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int tam, i;
+    float media, somatorio_rep, somatorio_rep_media = 0, somatorio_final;
+    float tamm, final, final_f, teste, val;
+    // ficou feio mas o que importa é que funciona KKKKKKKKKK
+    printf("Digite o tamanho do vetor: ");
+    
+    scanf("%d", &tam);
+    
+    int vet[tam];
+    
+    printf("\nDigite %d valores para compor o vetor.", tam);
+    printf("\n(A cada valor digitado aperte enter.)\n");
+    
+    for(i=0; i<tam; i++)
+    {
+        scanf("%d", &vet[i]);
+    }
+    for(i=0; i<tam; i++)
+    {
+        somatorio_rep_media = somatorio_rep_media + vet[i];
+    }
+    
+    media = (somatorio_rep_media / tam);
+    for(i=0; i<tam; i++)
+    {
+        val = vet[i]-media;
+        somatorio_rep = val*val;
+        somatorio_final = somatorio_final + somatorio_rep;
+    }
+    tamm = tam - 1;
+    final = somatorio_final/tamm;
+    teste = tam;
+    final_f = sqrt(final);
+    printf("O desvio padrão é: %.4f", final_f);
+    
+    return 0;
+}
