289f509a55b3f6fc8706f0f2ef5fe066b806b7d4
Baccus <103602285+BaccusIsCoding@users.noreply.github.com>
2022-06-05 11:15:00 -0300

    Create string1.c

---
+++ Atividades Aulas/Strings/string1.c
@@ -0,0 +1,8 @@
+// String é um vetor de caracteres:
+
+//char palavras[] = {"Vamos aprender a programar com C?"};
+
+// Imprimindo uam String:
+
+char palavras[] = {"Vamos aprender a programar com C?"};
+printf("%s", palavras);
