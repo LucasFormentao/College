29faec27be6a9e0f07946a122b1519233e8887f4
Baccus <103602285+BaccusIsCoding@users.noreply.github.com>
2022-06-05 12:06:06 -0300

    Update 1.c

--- Atividades Listas/Strings/ListaC05/1.c
+++ Atividades Listas/Strings/ListaC05/1.c
@@ -1,1 +1,13 @@
-a
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(){
+
+    char string[100];
+    printf("Digite qualquer texto: ");
+    fgets(string, 100, stdin);
+    printf("\nO texto digitado foi:\n%s", string);
+    
+    return 0;
+}
