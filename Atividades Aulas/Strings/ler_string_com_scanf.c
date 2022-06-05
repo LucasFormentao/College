020fb8fc0531443da8366a9e711715917bab4a09
Baccus <103602285+BaccusIsCoding@users.noreply.github.com>
2022-06-05 11:21:10 -0300

    Update ler_string_com_scanf.c

--- Atividades Aulas/Strings/ler_string_com_scanf.c
+++ Atividades Aulas/Strings/ler_string_com_scanf.c
@@ -7,6 +7,7 @@ int main()
     char palavras[100];
     printf("Digite o seu primeiro nome:");
     scanf("%s", palavras);
+    //No entanto, esse modelo no scanf para de ler após o primeiro espaço
     printf("%s", palavras);
 
     return 0;
