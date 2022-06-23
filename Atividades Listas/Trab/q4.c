87a2c1261330e997c220fe80e647ae76ebd01fb0
Baccus <103602285+BaccusIsCoding@users.noreply.github.com>
2022-06-23 12:48:32 -0300

    Create q4.c

---
+++ Atividades Listas/Trab/q4.c
@@ -0,0 +1,42 @@
+#include<stdlib.h>
+#include <stdio.h>
+
+int main()
+{
+    int saque, cem = 0, cinquenta = 0, vinte = 0, dez = 0;
+    printf("Digite o valor do saque: R$");
+    scanf("%d", &saque);
+    while(saque%10 != 0)
+    {
+        printf("Valor inválido, tente novamente: R$");
+        scanf("%d", &saque);
+    }
+    
+    do
+    {
+        if(saque >= 100)
+        {
+            cem++;
+            saque -= 100;
+        }
+        else if(saque >= 50)
+        {
+            cinquenta++;
+            saque -= 50;
+        }
+        else if(saque >= 20)
+        {
+            vinte++;
+            saque -= 20;
+        }
+        else if(saque >= 10)
+        {
+            dez++;
+            saque -= 10;
+        }
+    }
+    while (saque != 0);
+    printf("Notas entregue:\nR$100 --- %d\nR$50 ---- %d\nR$20 ---- %d\nR$10 ---- %d"\
+    , cem, cinquenta, vinte, dez);
+    return 0;
+}
