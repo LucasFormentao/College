80deb283dfeb10170420f64d93ea44c8f93c9408
Baccus <103602285+BaccusIsCoding@users.noreply.github.com>
2022-06-29 15:47:40 -0300

    Create teste_primeiro_struct.c

---
+++ Atividades Aulas/Struct/teste_primeiro_struct.c
@@ -0,0 +1,42 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef struct dados
+{
+    int matricula;
+    char nome, sobrenome;
+    float nota;
+};
+
+dados ler_dados(int x)
+{
+    for(i=0; i<x; i++)
+    {
+        dados aluno[x];
+        printf("Digite a matrícula do aluno:");
+        scanf("%d", &dados.matricula);
+        printf("Digite o nome do aluno:");
+        gets(dados.nome);
+        printf("Digite o sobrenome do aluno:");
+        gets(dados.sobrenome);
+        printf("Digite a nota do aluno:");
+        scanf("%f", &dados.nota);
+    }
+}
+
+int main()
+{
+    int qtd, i;
+    pessoa 
+    
+    printf("qtd alunos:");
+    scanf("%d", qtd);
+    dados nota[qtd];
+    
+    for(i=0; i>qtd; i++)
+    {
+        media = media + dados.nota
+    }
+
+    return 0;
+}
