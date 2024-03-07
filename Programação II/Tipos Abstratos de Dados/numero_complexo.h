c7826aaad3dc05722d9e0f66225419b5231b4754
Baccus <103602285+BaccusIsCoding@users.noreply.github.com>
2024-03-07 11:22:26 -0300

    numero_complexo.h

---
+++ Programação II/Tipos Abstratos de Dados/numero_complexo.h
@@ -0,0 +1,6 @@
+void Complexo_new (int qtd_cpx);
+void Complexo_end (int indice);
+void Complexo_sub (int qtd_cpx);
+void Complexo_add (int qtd_cpx);
+void Complexo_div (int qtd_cpx);
+void Complexo_mult (int qtd_cpx);
