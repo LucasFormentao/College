d77050b51c995c23336a1f3bb6f8b294888ca78d
Formentão <103602285+LucasFormentao@users.noreply.github.com>
2024-04-28 10:23:50 -0300

    Update agenda.c

--- Programação II/Atividade_Lista/agenda.c
+++ Programação II/Atividade_Lista/agenda.c
@@ -49,6 +49,14 @@ Contato ler_info(){
     return c;
 }
 
+void print_contato(Contato *info){
+    printf("\nNome: %s\n", info->nome);
+    printf("Telefone: %s\n", info->telefone);
+    printf("Celular: %s\n", info->celular);
+    printf("Email: %s\n", info->email);
+    printf("Aniversario: %d/%d\n", info->aniversario.dia, info->aniversario.mes);
+}
+
 void insere_contato(Agenda *agenda){
     No *aux, *novo = malloc(sizeof(No));
     if(novo){
@@ -69,6 +77,7 @@ void insere_contato(Agenda *agenda){
         novo->prox = aux->prox;
         aux->prox = novo;
         }
+        agenda->tam++;
     }
     else
         printf("Erro ao alocar memoria.\n");
@@ -88,7 +97,7 @@ void remover_duplicados(Agenda *agenda){
         else{
             aux = agenda->inicio;
             while(aux->prox){
-            while(strcmp(agenda->inicio->info.nome,string) != 0){
+            while(strcmp(aux->info.nome,string) != 0){
                 aux = aux->prox;
             }
             if(aux->prox){
@@ -101,3 +110,31 @@ void remover_duplicados(Agenda *agenda){
     }
 }
 
+void lista_contatos(Agenda *agenda){
+    int i;
+    No *aux;
+    aux = agenda->inicio;
+    printf("\n---------------Lista de Contatos (Tam: %d)---------------\n", agenda->tam);
+    for(i = 0; i<agenda->tam; i++){
+        print_contato(&aux->info);
+        printf("\n\n");
+        aux = aux->prox;
+    }
+}
+
+Contato busca_contato(Agenda *agenda){
+    char nome[40];
+    int i;
+    No *aux;
+    printf("\nDigite o nome do contato:\n");
+    scanf("%s", nome);
+
+    while(strcmp(aux->info.nome,nome) != 0){
+            aux = aux->prox;
+        }
+    if(aux->prox){
+        return aux->prox->info;
+    }
+    else
+        exit(0);
+}
