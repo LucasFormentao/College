85c575d38aeef851798a1e3a9ee23acd5b9dfdfd
Formentão <103602285+LucasFormentao@users.noreply.github.com>
2024-04-28 17:04:09 -0300

    Update header.h

--- Programação II/Atividade_Lista/header.h
+++ Programação II/Atividade_Lista/header.h
@@ -1,4 +1,14 @@
 typedef struct data Data;
 typedef struct contato Contato;
 typedef struct no No;
-typedef struct agenda Agenda;
+typedef struct lista Agenda;
+Agenda *criar_agenda();
+Contato ler_info();
+void print_contato(Contato *info);
+void insere_contato(Agenda *agenda);
+void remove_contato(Agenda *agenda, char *string);
+void lista_contatos(Agenda *agenda);
+No* busca_contato(Agenda *agenda, int i, char *nome);
+int conta_duplicados(Agenda *agenda, char *nome);
+void remove_duplicados(Agenda *agenda, char *nome);
+void atualiza_contato(Agenda *agenda, char *nome);
